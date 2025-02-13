ea492b396a2d0707b8c36ee9ed1ba0379bd55cd6
rayhankobir-dev <rayhankobir893@gmail.com>
2025-02-14 03:03:34 +0600

    fix: meta for home page

--- src/pages/dashboard/home.tsx
+++ src/pages/dashboard/home.tsx
@@ -1,10 +1,12 @@
-import { AreaChartDemo } from "@/components/charts/area-chart";
+import Meta from "@/components/common/meta";
 import { BarChartDemo } from "@/components/charts/bar-chart";
+import { AreaChartDemo } from "@/components/charts/area-chart";
 import { LineChartDemo } from "@/components/charts/line-chart";
 
 export default function HomePage() {
   return (
     <main className="space-y-4">
+      <Meta title="Dashboard" />
       <AreaChartDemo />
       <div className="grid md:grid-cols-2 gap-4">
         <BarChartDemo />
